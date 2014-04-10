a652eb27d5f33bd09ec66ae6af5574eb73212a24
Michael Messerli <michaelamesserli@gmail.com>
2014-04-10 21:08:10 +0200

    Added Partner Links & Donate on Teampost page

--- app/views/teamposts/index.blade.php
+++ app/views/teamposts/index.blade.php
@@ -15,6 +15,29 @@
     <div class="well">
       <a href="{{ action('TeampostController@create') }}" class="btn btn-primary">Add new Team</a>
     </div>
+    <h4>Partner Links</h4>
+    <div class="well">
+      <img src="{{ asset('img/ads/mumble.png') }}" alt="mumble logo" class="col-md-4 pull-left" />
+      <h4>Free Mumble Server!</h4>
+        <small>Found someone to play with but don't want to use in game voice? Check out this free to use Mumble server with your new party!</small>
+        <br><br><small><strong>IP: </strong>csgoreddit.mumble.com</small>
+        <br><small><strong>Port: </strong>5422</small>
+    </div>
+    <h4>Consider Donating</h4>
+    <div class="well">
+    <h5>Help keep CSGOTF alive</h5>
+      <small>If this site has helped you or you just want to help support the development, please consider leaving a small donation.</small>
+      <div class="col-md-6 col-md-offset-3 clearfix donate">
+        <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+        <input type="hidden" name="cmd" value="_s-xclick">
+        <input type="hidden" name="hosted_button_id" value="GDEBVYPPFHHLA">
+        <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+        <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+        </form>
+      </div>
+      <div class="clearfix"></div>
+    </div>
+
     <div class="well">
       <small>What do these Playstyle icons mean?</small>
       <br>
