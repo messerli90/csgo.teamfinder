6fa0ed89fcc89fa2ae1cc89183da5a32d14863fc
Michael Messerli <michaelamesserli@gmail.com>
2014-04-30 09:47:18 +0200

    Added demographics to Google Analytics

--- app/views/layouts/head.blade.php
+++ app/views/layouts/head.blade.php
@@ -26,6 +26,7 @@
 	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
 	  ga('create', 'UA-46522123-1', 'csgoteamfinder.com');
+	  ga('require', 'displayfeatures');
 	  ga('send', 'pageview');
 
 	</script>
